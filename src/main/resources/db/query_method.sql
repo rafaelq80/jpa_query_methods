2f1cc8f77dd6188cc9b27641a23fc319ad768cea
Rafael <rafaelproinfo@gmail.com>
2022-05-02 02:22:23 -0300

    Update

---
+++ src/main/resources/db/query_method.sql
@@ -0,0 +1,43 @@
+use db_query_methods;
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Maçã vermelha", "fruta", 1000, "Pomar Boas frutas", "2022-03-07", 1.99, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Maçã verde", "fruta", 1000, "Pomar Boas frutas", "2022-03-02", 2.99, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Maçã chilena", "fruta", 1000, "Pomar Só frutas", "2022-03-07", 3.99, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Banana prata", "fruta", 1300, "Pomar Flor de Minas", "2022-03-04", 9.90, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Banana nanica", "fruta", 1300, "Pomar Flor de Minas", "2022-03-05", 7.00, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Banana maçã", "fruta", 1300, "Pomar Flor de Minas", "2022-03-18", 12.00, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Banana da terra", "fruta", 1300, "Pomar Só frutas", "2022-03-28", 5.00, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Batata doce", "Legumes", 5000, "Sítio Legumes Feliz", "2022-03-10", 10.00, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Batata inglesa", "Legumes", 2050, "Oba Legumes", "2022-03-19", 7.50, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Batata asterisk", "Legumes", 25000, "Sítio Legumes Feliz", "2022-03-11", 15.00, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Batata inglesa premium", "Legumes", 25000, "Oba Legumes Feliz", "2022-03-11", 10.00, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Maçã vermelha", "fruta", 1000, "Pomar Só frutas", "2022-03-17", 4.99, " ", true);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Maçã vermelha", "fruta", 1500, "Pomar Flor de Goiás", "2022-03-21", 7.99, " ", false);
+
+INSERT INTO tb_produtos (nome, descricao, quantidade, fornecedor, data_validade, preco, foto, disponivel)
+VALUES ("Maçã", "fruta", 1500, "Pomar Flor de Goiás", "2022-04-21", 7.99, " ", false);
