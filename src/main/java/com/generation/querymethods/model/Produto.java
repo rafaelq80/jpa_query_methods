86c7008eb78d0fa3513d6124f081513067953215
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-04-02 04:38:07 -0300

    Update

--- src/main/java/com/generation/querymethods/model/Produto.java
+++ src/main/java/com/generation/querymethods/model/Produto.java
@@ -1,19 +1,19 @@
 package com.generation.querymethods.model;
 
-import java.time.LocalDate;
 import java.math.BigDecimal;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 @Table(name = "tb_produtos")
--- src/main/java/com/generation/querymethods/repository/ProdutoRepository.java
+++ src/main/java/com/generation/querymethods/repository/ProdutoRepository.java
@@ -8,11 +8,9 @@ import com.generation.querymethods.model.Produto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-    
+
     public List <Produto> findByPrecoLessThan(BigDecimal preco);
 
     public List <Produto> findByPrecoLessThanEqual(BigDecimal preco);
